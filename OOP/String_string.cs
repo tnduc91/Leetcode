18d409a15b114120a805ee6d29c316849668858b
Duke <tnduc91@gmail.com>
2020-06-24 12:23:06 +0800

    string vs String

---
+++ OOP/String_string.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Ref: https://www.youtube.com/watch?v=KAIYom46FCU&list=PLNLbdN-_vroNA4eWzJanw9ynCsAYY9KHp&index=3
+ * string is an alias of String
+ */
+using System;
+namespace LeetCode.OOP
+{
+    public class String_string
+    {
+        public void Test()
+        {
+            Console.WriteLine(typeof(string));
+            Console.WriteLine(typeof(String));
+
+            string name1 = "Duc 1"; // Work and good with coding convention
+            String name2 = "Duc 2"; // Work but not coding convention
+
+            Console.WriteLine(string.Concat(name1, name2));// Work but not coding convention
+            Console.WriteLine(String.Concat(name1, name2));// Work and good with coding convention
+        }
+    }
+}
